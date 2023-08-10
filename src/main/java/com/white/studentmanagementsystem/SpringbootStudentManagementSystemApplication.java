9545072bbf371d0191a2cf87694d11f2395d7c1b
yusa.turan <yusa.turan@iski.gov.tr>
2023-08-10 16:37:00 +0300

    Initial commit

---
+++ src/main/java/com/white/studentmanagementsystem/SpringbootStudentManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.white.studentmanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootStudentManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootStudentManagementSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/white/studentmanagementsystem/SpringbootStudentManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.white.studentmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootStudentManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
